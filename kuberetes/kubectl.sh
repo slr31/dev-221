aff735ded9c33af5e827f4ae72368e3d841cf837
slr31 <swathilr3001@gmail.com>
2026-04-05 00:55:28 +0530

    Create kubectl.sh

---
+++ kuberetes/kubectl.sh
@@ -0,0 +1,8 @@
+# Install Kind
+curl -Lo./kind https://kind.sigs.k8s.io/dl/v0.22.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+# Install kubectl
+curl-LO "https://l.k8s.io/release/Scurl-Ls https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x kubectl
+sudo my kubectl/usr/local/bin/
